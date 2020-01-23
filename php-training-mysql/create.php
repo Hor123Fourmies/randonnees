a8d544b53b8ebe0d278ec7c5faaeee3d1a38696a
Hor123 <hortensere@aol.com>
2020-01-23 12:33:09 +0100

    ajouter randonnees

---
+++ php-training-mysql/create.php
@@ -0,0 +1,90 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "reunion_island";
+
+$conn = new mysqli($servername, $username, $password);
+$conn->select_db($dbname);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Ajouter une randonnée</title>
+	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+	<a href="/php-pdo/read.php">Liste des données</a>
+	<h1>Ajouter</h1>
+	<form action="" method="post">
+		<div>
+			<label for="name">Name</label>
+			<input type="text" name="name" value="">
+		</div>
+
+		<div>
+			<label for="difficulty">Difficulté</label>
+			<select name="difficulty">
+				<option value="très facile">Très facile</option>
+				<option value="facile">Facile</option>
+				<option value="moyen">Moyen</option>
+				<option value="difficile">Difficile</option>
+				<option value="très difficile">Très difficile</option>
+			</select>
+		</div>
+		
+		<div>
+			<label for="distance">Distance</label>
+			<input type="text" name="distance" value="">
+		</div>
+		<div>
+			<label for="duration">Durée</label>
+			<input type="duration" name="duration" value="">
+		</div>
+		<div>
+			<label for="height_difference">Dénivelé</label>
+			<input type="text" name="height_difference" value="">
+		</div>
+		<button type="submit" name="button">Envoyer</button>
+	</form>
+</body>
+</html>
+
+
+<?php
+
+//echo $_POST ['difficulty'];
+//$difficulte = $_POST['difficulty'];
+
+// Requêtes préparées pr garantir ce qu'on attend :
+// (éviter les malveillances)
+// ? = placeholder à la place des valeurs
+// On n'indique pas l'ID car auto-incrementation
+
+$name = $_POST['name'];
+$difficulty = $_POST['difficulty'];
+$distance = $_POST['distance'];
+$duration = $_POST['duration'];
+$height_difference = $_POST['height_difference'];
+
+
+$stmt = $conn->prepare("INSERT INTO hiking (name, difficulty, distance, duration, height_difference) VALUES (?,?,?,?,?)");
+
+$stmt->bind_param("ssiii", $name, $difficulty, $distance, $duration, $height_difference);
+
+
+if($stmt->execute()){
+    print "La randonnée <span style='font-weight: bold'>$name</span> a été ajoutée.";
+}else{
+    print $conn->error;
+}
+
+
+//$stmt->execute();
+
+$stmt->close();
+
+?>
---
+++ php-training-mysql/read.php
@@ -0,0 +1,67 @@
+<link rel="stylesheet" href="basics.css">
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "reunion_island";
+
+$conn = new mysqli($servername, $username, $password);
+$conn->select_db($dbname);
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Randonnées</title>
+    <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <h1>Liste des randonnées</h1>
+    <table>
+      <!-- Afficher la liste des randonnées -->
+        <?php
+
+                $randonnees = "SELECT * FROM hiking WHERE 1";
+                $result = $conn->query($randonnees);
+                echo $conn->error;
+
+        ?>
+                        <tr>
+                            <th scope="col">Nom de la randonnée</th>
+                            <th scope="col">Difficulté</th>
+                            <th scope="col">Distance</th>
+                            <th scope="col">Durée</th>
+                            <th scope="col">Dénivelé</th>
+                        </tr>
+        <?php
+
+                    while ($row = $result->fetch_assoc()) {
+
+                        ?>
+
+                        <tr>
+                            <td>
+                                <?php echo $row['name'] . "<br>";?>
+                            </td>
+                            <td>
+                                <?php echo $row['difficulty'] . "<br>";?>
+                            </td>
+                            <td>
+                                <?php echo $row['distance'] . "<br>";?>
+                            </td>
+                            <td>
+                                <?php echo $row['duration'] . "<br>";?>
+                            </td>
+                            <td>
+                                <?php echo $row['height_difference'] . "<br>";?>
+                            </td>
+                        </tr>
+                            <?php
+                    }
+?>
+    </table>
+  </body>
+</html>
+
